6852b144dfc94ac4e3506c310d8ffe9b735217e6
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-09-10 19:25:13 -0400

    creating canvas and adding color

--- topics/instructions/landscape-challenge/js/script.js
+++ topics/instructions/landscape-challenge/js/script.js
@@ -1,8 +1,9 @@
 /**
- * Title of Project
- * Author Name
+ * Title of Project landscape-challenge
+ * Author Name Ziyan Pan
+ * group member Weini Wang
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * HOW EMBARRASSING! Draw a landscape with a house 
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
  */
 
@@ -12,7 +13,8 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640, 480);//creating ur canvas
+    background("#80d6f1");
 }
 
 
