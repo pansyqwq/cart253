23c0e8ddeda4be034a682e3006464c2e7b9fe190
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-09-10 19:58:41 -0400

    added a cloud and fixed bug
    added a gray could and fixed push and pop bug

--- topics/instructions/landscape-challenge/js/script.js
+++ topics/instructions/landscape-challenge/js/script.js
@@ -25,14 +25,33 @@ function setup() {
 */
 function draw() {
     land();
+    cloud();
 }
 function land(){
-    ellipse(150,500,450,300);//ellipse(x, y, width, height), x, y is the position in the canvas
+    push();
     fill("#007ca5");//giving the ellipse color
     noStroke();//no border line 
+    ellipse(150,500,450,300);//ellipse(x, y, width, height), x, y is the position in the canvas
+    pop();
 
-    ellipse(430,550,500,300);// the second ellipse 
+    push();
     fill("#157291")//darker color 
     noStroke();
+    ellipse(430,550,500,300);// the second ellipse 
+    pop();
 
+}
+
+function cloud(){
+
+    //a clound learned from lecture, from pippin. location and color changed 
+    push();
+    noStroke();
+    fill("#c7c7c7");
+    ellipse(400,100,100,100);
+    ellipse(480,80,100,100);
+    ellipse(460,120,60,60);
+    ellipse(490,130,60,60);
+    ellipse(520,120,60,60);
+    pop();
 }
