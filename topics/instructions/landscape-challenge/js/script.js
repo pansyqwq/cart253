647fdf8e52445e1106a8ea0c6d65938dc3fbfcb5
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-09-10 20:53:35 -0400

    added a nice house

--- topics/instructions/landscape-challenge/js/script.js
+++ topics/instructions/landscape-challenge/js/script.js
@@ -30,58 +30,112 @@ function draw() {
     cloud();
     moon();
     star();
-
+    drawHouse();
 }
-function land(){
+function land() {
     push();
     fill("#007ca5");//giving the ellipse color
     noStroke();//no border line 
-    ellipse(150,500,450,300);//ellipse(x, y, width, height), x, y is the position in the canvas
+    ellipse(150, 500, 450, 300);//ellipse(x, y, width, height), x, y is the position in the canvas
     pop();
 
     push();
-    fill("#157291")//darker color 
+    fill("#157291");//darker color 
     noStroke();
-    ellipse(430,550,500,300);// the second ellipse 
+    ellipse(435, 550, 580, 300);// the second ellipse 
     pop();
 
 }
 
-function cloud(){
+function cloud() {
     //a clound learned from lecture, from pippin. location and color changed 
     push();
     noStroke();
     fill("#c7c7c7");
-    ellipse(400,100,100,100);
-    ellipse(480,80,100,100);
-    ellipse(460,120,60,60);
-    ellipse(490,130,60,60);
-    ellipse(520,120,60,60);
+    ellipse(400, 100, 100, 100);
+    ellipse(480, 80, 100, 100);
+    ellipse(460, 120, 60, 60);
+    ellipse(490, 130, 60, 60);
+    ellipse(520, 120, 60, 60);
     pop();
 }
 
-function moon(){ 
+function moon() {
     //adding a white circle as moon 
     push();
     noStroke();
     fill(255);
-    ellipse(100,100,120,120);
+    ellipse(100, 100, 120, 120);
     pop();
 }
 
-function star(){ //a diamond shape star 
+function star() { //a diamond shape star 
     push();
     noStroke();
-    fill("#c2f1ed")
-    quad(230,122,246,80,230,38,214,80);
+    fill("#c2f1ed");
+    quad(230, 122, 246, 80, 230, 38, 214, 80);
     pop();
 }
 
-function people(){ //using a circle and a ellipse as the body and head of the character 
+function people() { //using a circle and a ellipse as the body and head of the character 
     push();
     noStroke();
     fill("#32327c")
-    ellipse(100,300,20,20);
-    ellipse(100,350,20,90);
+    ellipse(100, 300, 20, 20);
+    ellipse(100, 350, 20, 90);
+    pop();
+}
+
+//the house part inspiration from the lecture 
+function drawHouse() {
+    drawBody();
+    drawRoof();
+    drawWindow();
+    drawDoor();
+}
+
+//draw the body of the house by a rectangle
+function drawBody() {
+    // The main body of the house
+    push();
+    noStroke();
+    fill("#3f3f87");
+    rect(300, 240, 280, 180);
+    pop();
+}
+
+//Draws the roof of our house (a triangle)
+function drawRoof() {
+    push();
+    noStroke();
+    fill("#32327c");
+    triangle(280, 240, 440, 180, 600, 240);
+    pop();
+}
+
+// Draws a window on our house
+ 
+function drawWindow() {
+    push();
+    noStroke();
+    fill("#8888bb");
+    ellipse(500, 320, 80, 80);
+    pop();
+}
+
+//added door on our project 
+function drawDoor() {
+    // The door
+    push();
+    noStroke();
+    fill("#7676b1");
+    rect(320, 300, 80, 120);
+    pop();
+
+    // The doorknob
+    push();
+    noStroke();
+    fill("#32327c");
+    ellipse(340, 360, 10, 10);
     pop();
 }
