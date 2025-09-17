100bcb8f77e70b67393f5e9a3f5d1d3ed4169bac
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-09-17 18:10:59 -0400

    added a new file

---
+++ creating-variables/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(400,600);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(0);
+    push();
+    //color of ellipse 
+    fill(255,0,0);
+    //x,y,width, height
+    ellipse(mouseX,mouseY,50,50)
+    pop();
+}
