c9977951a5e406524d6cad593f81694c5672935e
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-09-17 19:30:42 -0400

    changed project name

--- variables_challenge/js/script.js
+++ variables_challenge/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Mr. Furious Furious
+ * Ziyan Pan
+ * working with Weini Wang
+ * variables challenge
  */
 
 "use strict";
