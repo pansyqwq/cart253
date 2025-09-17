35db391b6a75a5c5293f44dd31b515b86984cf58
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-09-17 19:46:40 -0400

    make the circle more red

--- variables_challenge/js/script.js
+++ variables_challenge/js/script.js
@@ -7,17 +7,48 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225,
+    reduceG:1,
+    reduceB:1
+  }
+};
 
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
+  background(160, 180, 200);
 
+  console.log(mrFurious.fill.g);
+  // make the circle more and more red
+  mrFurious.fill.g = mrFurious.fill.g - mrFurious.fill.reduceG;
+  mrFurious.fill.b = mrFurious.fill.b - mrFurious.fill.reduceB;
+  //set the range of the color
+  mrFurious.fill.g = constrain(mrFurious.fill.g,0,255);
+  mrFurious.fill.b = constrain(mrFurious.fill.b,0,255);
+  
+
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
 }
