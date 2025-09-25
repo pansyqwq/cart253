17ba9d335ed81b7dd012280b0ef536ef3fa48bd4
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-09-24 22:58:09 -0400

    creating half of a petal

--- ArtJam/js/script.js
+++ ArtJam/js/script.js
@@ -21,6 +21,19 @@ const pistil = {
   }
 };
 
+const petal = {
+  x1: undefined,
+  y1: undefined,
+  x2: undefined,
+  y2: undefined,
+  x3: undefined,
+  y3: undefined,
+  fills: {
+    dark: "#0f0142ff", // white for large triangle
+    light: "#4332ffff" // yellow for small triangle
+  }
+}
+
 /**
  * CREATING MY CANVAS
 */
@@ -29,11 +42,11 @@ function setup() {
     background("#80d6f1");
 }
 
-
 /**
  * all the functions that builds the portrait
 */
 function draw() {
+    drawPetal();
     drawPistil();
 }
 
@@ -64,4 +77,22 @@ function drawPistil(){
     pistil.y3 = pistil.y2;
     triangle(pistil.x1, pistil.y1, pistil.x2, pistil.y2, pistil.x3, pistil.y3); 
     pop();
+}
+
+function drawPetal(){
+    push();
+    noStroke();
+    fill(petal.fills.dark);
+    petal.x1 = 400;
+    petal.y1 = 400;
+    petal.x2 = petal.x1 + 100;
+    petal.y2 = petal.y1 + 100;
+    petal.x3 = petal.x1 - 100;
+    petal.y3 = petal.y2;
+    triangle(petal.x1, petal.y1, petal.x2, petal.y2, petal.x3, petal.y3); 
+    pop();
+
+    push();
+    fill(petal.fills.dark);
+    arc(348, 530, 120, 100, 0, PI + QUARTER_PI, OPEN);
 }
