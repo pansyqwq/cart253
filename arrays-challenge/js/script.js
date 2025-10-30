a2da40f82981fd0f3768c3871d0b9cb126bb87a1
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-10-29 20:02:48 -0400

    add a for loop to draw

--- arrays-challenge/js/script.js
+++ arrays-challenge/js/script.js
@@ -12,84 +12,89 @@
  */
 
 let ball = undefined; // Will create it with createBall()
-let balloon = [];// creating an empty array for balls
+let balloons = [];// creating an empty array for balls
 
 /**
  * Create the canvas and the ball
  */
 function setup() {
-  // Create the canvas
-  createCanvas(400, 400);
-  // Create the ball
-  ball = createBall();
+    // Create the canvas
+    createCanvas(400, 400);
+    // Create the ball
+    ball = createBall();
 }
 
 /**
  * Creates a random ball
  */
 function createBall() {
-  // Create a ball object with appropriate properties
-  const newBall = {
-    // Position and dimensions
-    x: 200,
-    y: 200,
-    size: 20,
-    // Colour
-    fill: "#000000",
-    // Movement
-    velocity: {
-      x: random(-5, 5),
-      y: random(-5, 5)
-    }
-  };
-  return newBall;
+    // Create a ball object with appropriate properties
+    const newBall = {
+        // Position and dimensions
+        x: 200,
+        y: 200,
+        size: 20,
+        // Colour
+        fill: "#000000",
+        // Movement
+        velocity: {
+            x: random(-5, 5),
+            y: random(-5, 5)
+        }
+    };
+    return newBall;
 }
 
 /**
  * Moves and draws the ball
  */
 function draw() {
-  background("#87ceeb");
-  
-  moveBall(ball);
-  bounceBall(ball);
-  drawBall(ball);
+    background("#87ceeb");
+
+
+
+    for (let ball of balloons) {
+        moveBall(ball);
+        bounceBall(ball);
+        drawBall(ball);
+    }
+
 }
 
 /**
  * Moves the ball according to its velocity
  */
 function moveBall(ball) { //Convert to functions with parameters
-  ball.x += ball.velocity.x;
-  ball.y += ball.velocity.y;
+    ball.x += ball.velocity.x;
+    ball.y += ball.velocity.y;
 }
 
 /**
  * Bounces the ball off the walls
  */
 function bounceBall() {
-  // Check if the ball has reached the left or right
-  const bounceX = (ball.x > width || ball.x < 0);
-  // Check if the ball has reached the top or bottom
-  const bounceY = (ball.y > height || ball.y < 0);
-  
-  // Handle bouncing horizontally
-  if (bounceX) {
-    ball.velocity.x *= -1;
-  }
-  // Handle bouncing vertically
-  if (bounceY) {
-    ball.velocity.y *= -1;
-  }
+    // Check if the ball has reached the left or right
+    const bounceX = (ball.x > width || ball.x < 0);
+    // Check if the ball has reached the top or bottom
+    const bounceY = (ball.y > height || ball.y < 0);
+
+    // Handle bouncing horizontally
+    if (bounceX) {
+        ball.velocity.x *= -1;
+    }
+    // Handle bouncing vertically
+    if (bounceY) {
+        ball.velocity.y *= -1;
+    }
 }
 
 /**
  * Draw the ball on the canvas
  */
 function drawBall(ball) {
-  push();
-  noStroke();
-  fill(ball.fill);
-  ellipse(ball.x, ball.y, ball.size);
-  pop();
+    push();
+    noStroke();
+    fill(ball.fill);
+    ellipse(ball.x, ball.y, ball.size);
+    pop();
 }
