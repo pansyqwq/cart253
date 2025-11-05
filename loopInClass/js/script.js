ce6ee4dbe21f4b156eb3d32a2fba2f16f701e9d0
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-11-05 18:30:37 -0500

    in class, working in for loops

---
+++ loopInClass/js/script.js
@@ -0,0 +1,46 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(500,500);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(0);
+    // let y = height/2;
+    let x = 0;
+    let rectWidth = 50;
+    // fill(255,0,0);
+
+    // while(x < width){
+    //     rect(x, y, rectWidth);
+    //     x = x + rectWidth;
+    // }
+
+    // let x_2 = 10;
+    // while (y < height){
+    //     rect(x_2, y, rectWidth);
+    // }
+    // for(let i=20; i>=0; i--){// don't use i=0 anywhere else
+    //     console.log(i);
+    // }
+
+    for(let y =0; y<height; y = y+ rectWidth){
+        fill(255,0,0);
+        rect(x, y, rectWidth);
+    }
+}
