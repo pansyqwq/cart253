2ef207fda0283253da5357f957b898ee76181e08
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-11-05 19:26:42 -0500

    added title

--- loops-challenge/js/script.js
+++ loops-challenge/js/script.js
@@ -1,24 +1,56 @@
 /**
- * Title of Project
- * Author Name
+ * Lines
+ * Ziyan Pan, teamate: Weini Wang
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A series of lines across the canvas
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Creates the canvas
+ */
 function setup() {
-
+    createCanvas(500, 500);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draws lines across the canvas with increasing thickness and
+ * gradually lightening colour
+ */
 function draw() {
-
+    background("pink");
+    //line(x1, y1, x2, y2)
+    stroke(0);
+    line(0, 0, 0, height);
+    
+    stroke(25);
+    line(50, 0, 50, height);
+    
+    stroke(50);
+    line(100, 0, 100, height);
+    
+    stroke(75);
+    line(150, 0, 150, height);
+    
+    stroke(100);
+    line(200, 0, 200, height);
+    
+    stroke(125);
+    line(250, 0, 250, height);
+    
+    stroke(150);
+    line(300, 0, 300, height);
+    
+    stroke(175);
+    line(350, 0, 350, height);
+    
+    stroke(200);
+    line(400, 0, 400, height);
+    
+    stroke(225);
+    line(450, 0, 450, height);
+    
+    stroke(250);
+    line(500, 0, 500, height);
 }
