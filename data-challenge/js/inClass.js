5da1b78bab8ee37998d451092843ff8ac62bd5cd
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-11-12 18:34:20 -0500

    Update inClass.js

---
+++ data-challenge/js/inClass.js
@@ -0,0 +1,14 @@
+"use district";
+const CELL_SIZE = 125;
+const NUM_COLS = 4;
+const NUM_ROWS = 4;
+let currentMaze = null;
+
+
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+    background(0);
+}
