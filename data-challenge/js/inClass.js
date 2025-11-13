a92fdae197f7358321867860f80905a5a3cf2df1
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2025-11-12 19:33:15 -0500

    debugged the color

--- data-challenge/js/inClass.js
+++ data-challenge/js/inClass.js
@@ -1,19 +1,20 @@
 "use district";
-const CELL_SIZE = 125;
-const NUM_COLS = 4;
-const NUM_ROWS = 4;
+let CELL_SIZE = 125;
+let NUM_COLS = 4;
+let NUM_ROWS = 4;
 let currentMaze = null;
 let jsonDATA;
+let currentMazeObject = null;
+let counter = 0;
 
-function preload(){
+function preload() {
     jsonDATA = loadJSON("assets/data/mazeClass.json");
     console.log(jsonDATA);
 }
 function setup() {
     createCanvas(500, 500);
+    mousePressed();
 
-    //get first maze
-    currentMaze = jsonDATA.mazes[0]
     console.log(currentMaze);
 }
 
@@ -25,15 +26,30 @@ function draw() {
 function drawMaze() {
     for (let i = 0; i < NUM_COLS; i++) {
         for (let j = 0; j < NUM_ROWS; j++) {
-            if(currentMaze[i][j] ===1){
-            drawCell(i,j);// call i and j here
+            if (currentMaze[i][j] === 1) {
+                drawCell(i, j);// call i and j here
             }
         }
     }
 }
 
-function drawCell(x,y) {
-    fill("#FF0000");
+function drawCell(x, y) {
+    fill(`#${currentMazeObject.color}`);
     rectMode(CORNER);
     rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+}
+
+function mousePressed() {
+    counter++;
+    // if (counter > 2) { counter = 0 }
+    //get first maze
+    currentMazeObject = jsonDATA.mazes[counter%3]// number in here changes the set of data in maze json, % 3 take the remainder of 3
+    currentMaze = currentMazeObject.maze;
+    reSet();
+}
+
+function reSet() {
+    NUM_COLS = currentMaze.length;
+    NUM_ROWS = currentMaze.length;
+    CELL_SIZE = width / currentMaze.length;
 }
